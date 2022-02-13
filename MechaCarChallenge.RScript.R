1ce9f50425894f78ec1d4eed93e4e84176116d36
Stephen Joseph Weatherbee <sjweatherbee@Stephens-MacBook-Pro.local>
2022-02-13 10:56:01 -0800

    adding Deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -12,3 +12,13 @@ susp_coil <- read.csv(file='Suspension_Coil.csv')
 total_summary <- susp_coil %>% summarize(Avg_PSI=mean(PSI),median_PSI=median(PSI),variance=var(PSI),StandardDeviation=sd(PSI), .groups = 'keep')
 
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSIe=mean(PSI),median_PSI=median(PSI),variance=var(PSI),StandardDeviation=sd(PSI), .groups = 'keep')
+
+t.test(log10(susp_coil$PSI),mu=mean(log10(lot_summary$Mean_PSIe)))
+
+?t.test
+
+t.test(subset(susp_coil$PSI,susp_coil$Manufacturing_Lot == "Lot1"))
+
+t.test(subset(susp_coil$PSI,susp_coil$Manufacturing_Lot == "Lot2"))
+
+t.test(subset(susp_coil$PSI,susp_coil$Manufacturing_Lot == "Lot3"))
