310210799b28c70d4ff5cdf7b3c86ae0dc466648
Stephen Joseph Weatherbee <sjweatherbee@Stephens-MacBook-Pro.local>
2022-02-14 13:22:57 -0800

    Edit Deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -13,12 +13,20 @@ total_summary <- susp_coil %>% summarize(Avg_PSI=mean(PSI),median_PSI=median(PSI
 
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSIe=mean(PSI),median_PSI=median(PSI),variance=var(PSI),StandardDeviation=sd(PSI), .groups = 'keep')
 
-t.test(susp_coil$PSI,lot_summary$Mean_PSIe)
+t.test(susp_coil$PSI, mu=1500)
 
 ?t.test
 
-t.test(subset(susp_coil$PSI,susp_coil$Manufacturing_Lot == "Lot1"))
+L1 <- subset(susp_coil, Manufacturing_Lot == "Lot1")
+L2 <- subset(susp_coil, Manufacturing_Lot == "Lot2")
+L3 <- subset(susp_coil, Manufacturing_Lot == "Lot3")
 
-t.test(subset(susp_coil$PSI,susp_coil$Manufacturing_Lot == "Lot2"))
+t.test(L1$PSI)
+#Comparing sample vs population
 
-t.test(subset(susp_coil$PSI,susp_coil$Manufacturing_Lot == "Lot3"))
+t.test((susp_coil$PSI), mu=mean(L1$PSI))
+
+t.test((susp_coil$PSI), mu=mean(L2$PSI))
+
+t.test((susp_coil$PSI), mu=mean(L3$PSI))
+
